6bd64a76e694549d02d5791789f5b546ad05ddee
KelSolaar <KelSolaar@192.168.0.11>
2009-07-03 02:14:11 +0200

    sIBL_GUI - 2.0.0 - Change Log -
    - VRay For Maya Template Updated.
    - New External Tool : sIBL_GUI_Gather_Templates.sh.

---
+++ src/External Tools/sIBL_GUI_Gather_Templates.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+echo ----------------------------------------------------------------
+echo sIBL_GUI - Templates Gathering
+echo ----------------------------------------------------------------
+
+#! Gathering Folder Cleanup.
+rm -rf ./Releases/Gathering/Templates/*
+
+#! Templates Gathering.
+cp -rf ./Templates ./Releases/Gathering/
+
+#! Releases File Gathering.
+cp ./Releases/sIBL_GUI_Releases.rc ./Releases/Gathering/
+
+#! Gathering Folder Cleanup.
+python ./External\ Tools/KSL_Recursive_Remove.py ./Releases/Gathering/Templates/ .DS_Store
