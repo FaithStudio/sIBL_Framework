a7a06930fe930a4b99766384106864baa8333548
KelSolaar <KelSolaar@192.168.0.2>
2009-07-12 09:24:41 +0200

    sIBL_GUI - 2.0.2 - Change Log -
    - External Tools Updates.

--- src/External Tools/sIBL_GUI_Gather_Templates.sh
+++ src/External Tools/sIBL_GUI_Gather_Templates.sh
@@ -14,3 +14,4 @@ cp ./Releases/sIBL_GUI_Releases.rc ./Releases/Gathering/
 
 #! Gathering Folder Cleanup.
 python ./External\ Tools/KSL_Recursive_Remove.py ./Releases/Gathering/Templates/ .DS_Store
+python ./External\ Tools/KSL_Recursive_Remove.py ./Releases/Gathering/Templates/ Thumbs.db
